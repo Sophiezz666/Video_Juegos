3961a146b3f5408d336362cfc1913463b7105ae5
Luis Felipe Hinestroza Pinto <felipepromil@gmail.com>
2025-12-03 10:59:29 -0500

    feat:se agrego clase videojuego en videojuego.py

---
+++ VideoJuego.py
@@ -0,0 +1,13 @@
+
+class VideoJuego:
+    def __init__(self,id,title,genre,platform,release_year,units_sold,metascore,total_revenue):
+        self.id = id
+        self.title = title
+        self.genre = genre
+        self.platform = platform
+        self.platform = platform
+        self.release_year = release_year
+        self.units_sold = units_sold
+        self.metascore = metascore
+        self.total_revenue = total_revenue
+
